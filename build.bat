1b88118b9f8ad587661ee8bd3bf791342f226305
Karl Eklund <localpart@gmail.com>
2020-10-09 01:41:20 +0200

    Add Erlang include files here instead

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\bin\Hostx64\x64\cl.exe" -I C:\Users\karle\scoop\apps\erlang\current\usr\include -LD -MD -Fe complex6_nif.c complex6.c
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\bin\Hostx64\x64\cl.exe" -I include -LD -MD -Fe complex6_nif.c complex6.c
